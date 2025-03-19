955dc231fdcb5a30b5f0cd9977e199e22e95ec57
Jessie Kelly <jessie.kelly@stud.h-da.de>
2025-03-19 19:04:05 +0000

    Opening Animations

---
+++ Assets/FadeMaterialManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeMaterialManager : MonoBehaviour
+{
+    private RawImage _rawImage;
+    private Material _renderMat;
+    public float updateSpeed;
+
+    void Start()
+    {
+        _rawImage = GetComponent<RawImage>();
+        _renderMat = _rawImage.materialForRendering;
+    }
+
+    public void FadeMaterialIn()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeMaterialOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        for (float i = 0f; i <= 1f; i += updateSpeed)
+        {
+            _renderMat.SetFloat("_Alpha", i);
+            yield return new WaitForFixedUpdate();
+        }
+        _renderMat.SetFloat("_Alpha", 1f);
+    }
+    
+    private IEnumerator FadeOut()
+    {
+        for (float i = 1f; i >= 0f; i -= updateSpeed)
+        {
+            _renderMat.SetFloat("_Alpha", i);
+            yield return new WaitForFixedUpdate();
+        }
+        _renderMat.SetFloat("_Alpha", 0f);
+        
+    }
+}
