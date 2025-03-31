30febbe6a24dc7e5d4d3dd6b6122bae4a05e929f
Jessie Kelly <jessie.kelly@stud.h-da.de>
2025-03-31 14:53:29 +0100

    Text Fade in

--- Assets/Scripts/Dialogue/BubbleObject.cs
+++ Assets/Scripts/Dialogue/BubbleObject.cs
@@ -18,13 +18,18 @@ public class BubbleObject : MonoBehaviour
     {
         for (int i = 0; i <= textToShow.Length; i++)
         {
-            bubbleText.text = textToShow.Insert(i, "<alpha=#00>");
+            // Text fade in
+            var tempText = textToShow;
+            tempText = tempText.Insert(i, "<alpha=#00>");
+            tempText = tempText.Insert(Mathf.Max(i -1, 0), "<alpha=#44>");
+            tempText = tempText.Insert(Mathf.Max(i -2, 0), "<alpha=#66>");
+
+            bubbleText.text = tempText;
             yield return new WaitForSeconds(0.05f);
         }
-
+        bubbleText.text = textToShow;
         yield return null;
     }
-
     public void SkipText(string textToShow)
     {
         StopCoroutine(coroutine);
